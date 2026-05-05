95e079e6cdebbad3f1e74151928f1f6a4adf939f
AndreLuizdeLima <andreluizjp2015@gmail.com>
2026-05-04 22:40:59 -0300

    quarto commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,7 @@
 import { Module } from '@nestjs/common';
-import { Pool } from 'pg';
-import Redis from 'ioredis';
 import { UsersModule } from './users/users.module';
 
 @Module({
     imports: [UsersModule],
-    providers: [
-        {
-            provide: 'PG_CONNECTION',
-            useFactory: (): any => {
-                return new Pool({
-                    host: 'localhost',
-                    port: 5432,
-                    user: 'postgres',
-                    password: 'admin',
-                    database: 'test',
-                });
-            },
-        },
-        {
-            provide: 'REDIS_CLIENT',
-            useFactory: (): any => {
-                return new Redis({
-                    host: 'localhost',
-                    port: 6379,
-                });
-            },
-        },
-    ],
-    exports: ['PG_CONNECTION', 'REDIS_CLIENT'],
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
+import { Pool } from 'pg';
+import Redis from 'ioredis';
+
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
-    providers: [UsersService],
     controllers: [UsersController],
+    providers: [
+        UsersService,
+
+        // 🐘 PostgreSQL
+        {
+            provide: 'PG_CONNECTION',
+            useFactory: () => {
+                return new Pool({
+                    host: 'localhost',
+                    port: 5432,
+                    user: 'postgres',
+                    password: 'admin',
+                    database: 'teste',
+                });
+            },
+        },
+
+        // 🔴 Redis
+        {
+            provide: 'REDIS_CLIENT',
+            useFactory: () => {
+                return new Redis({
+                    host: 'localhost',
+                    port: 6379,
+                    password: 'admin',
+                });
+            },
+        },
+    ],
 })
 export class UsersModule {}
